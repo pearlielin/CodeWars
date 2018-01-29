1fdfd927de29f3053f51eb176ca1cd4e0580c951
pearlie <pearlielin11@gmail.com>
2018-01-29 23:24:36 +0800

    pass wildcard test case

---
+++ CodeWars/WildCards.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using NUnit.Framework.Interfaces;
+
+namespace CodeWars
+{
+    public class WildCards
+    {
+        public List<string> Possibilities(string input)
+        {
+            var newlist = new List<string>();
+            var templist = new List<string>();
+            var count = input.Count(t => t == '?');
+            var regex = new Regex(Regex.Escape("?"));
+            newlist.Add(regex.Replace(input, "0", 1));
+            newlist.Add(regex.Replace(input, "1", 1));
+
+            NewMethod(newlist,count);
+
+            return newlist;
+        }
+
+        private static void NewMethod(List<string> newlist,int count)
+        {
+            var regex = new Regex(Regex.Escape("?"));
+            for(var i=0;i<=Math.Pow(2,count);i++)
+            {
+                foreach (var item in newlist)
+                {
+                    if (item.Contains("?"))
+                    {
+                        newlist.Add(regex.Replace(item, "0", 1));
+                        newlist.Add(regex.Replace(item, "1", 1));
+                        newlist.Remove(item);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CodeWarsTests/WildCardsTest.cs
@@ -0,0 +1,32 @@
+﻿using CodeWars;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWarsTests
+{
+    [TestFixture]
+    internal class WildCardsTest
+    {
+        [Test]
+        public void Basic1()
+        {
+            var list = new List<string> { "1001", "1011" };
+            Assert.AreEqual(new WildCards().Possibilities("10?1").OrderBy(t => t), list.OrderBy(t => t));
+        }
+
+        [Test]
+        public void Basic2()
+        {
+            var list = new List<string> { "1001", "1011", "1000", "1010" };
+            Assert.AreEqual(new WildCards().Possibilities("10??").OrderBy(t => t), list.OrderBy(t => t));
+        }
+
+        [Test]
+        public void Basic3()
+        {
+            var list = new List<string> { "0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111" };
+            Assert.AreEqual(new WildCards().Possibilities("????").OrderBy(t => t), list.OrderBy(t => t));
+        }
+    }
+}
